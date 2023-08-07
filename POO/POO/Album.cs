a274ceaf9121c0a3ffbd768551b065767b9e53e3
André Toti <detoti@gmail.com>
2023-08-07 14:53:13 -0300

    Updating some studies

--- POO/POO/Album.cs
+++ POO/POO/Album.cs
@@ -2,13 +2,29 @@
 {
 	private List<Musica> musicas = new List<Musica>();
 
-	public string Nome { get; set; }
+    public Album(string nome)
+    {
+        Nome = nome;
+    }
 
-	public int DuracaoTotal { get; set; }
+    public string Nome { get; }
+
+	public int DuracaoTotal => musicas.Sum(m => m.Duracao);
 
 	public void AdicionarMusica(Musica musica)
 	{
 		musicas.Add(musica);
 	}
+
+	public void ExibirMusicasAlbum()
+	{
+		Console.WriteLine($"Lista de Musicas do album {Nome}: \n");
+		foreach (var musica in musicas)
+		{
+            Console.WriteLine($"Musica: {musica.Nome}");
+
+        }
+		Console.WriteLine($"\nPara ouvir o album inteiro voce precisa de {DuracaoTotal} segundos.");
+    }
 }
 	
--- POO/POO/Program.cs
+++ POO/POO/Program.cs
@@ -1,16 +1,57 @@
-﻿Musica musica1 = new Musica();
-musica1.Nome = "All In Gang";
-musica1.Artista = "Cone Crew Diretoria";
-musica1.Duracao = 230;
-musica1.Disponivel = (true);
-Console.WriteLine(musica1.Disponivel);
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        //Banda cone = new Banda("Cone Crew Diretoria");
 
-Musica musica2 = new Musica();
-musica2.Nome = "Calma na Alma";
-musica2.Artista = "Cone Crew Diretoria";
-musica2.Duracao = 317;
-musica2.Disponivel = (false);
-Console.WriteLine(musica2.Disponivel);
+        //Album albumDaCone = new Album("Bonde da Madrugada pt.1");
 
-musica1.ExibirFichaTecnica();
-musica2.ExibirFichaTecnica();
+        //Musica musica1 = new Musica(cone, "To de Volta no Twist")
+        //{
+        //    Duracao = 226,
+        //    Disponivel = true
+        //};
+
+        //Musica musica2 = new Musica(cone, "Chefe de Quadrilha")
+        //{
+        //    Duracao = 278,
+        //    Disponivel = false,
+        //};
+
+        //Musica musica3 = new Musica(cone, "Pra Minha Mãe")
+        //{
+        //    Duracao = 332,
+        //    Disponivel = true,
+        //};
+
+
+        //albumDaCone.AdicionarMusica(musica1);
+        //albumDaCone.AdicionarMusica(musica2);
+        //albumDaCone.AdicionarMusica(musica3);
+        //cone.AdicionarAlbum(albumDaCone);
+
+        //musica1.ExibirFichaTecnica();
+        //musica2.ExibirFichaTecnica();
+        //musica3.ExibirFichaTecnica();
+        //albumDaCone.ExibirMusicasAlbum();
+        //cone.ExibirDiscografia();
+
+        Episodio ep1 = new(1, "Cone Crew", 45);
+        ep1.AdicionarConvidado("Rany Money");
+        ep1.AdicionarConvidado("Batoré");
+
+        Episodio ep2 = new(2, "N.W.E", 120);
+        ep2.AdicionarConvidado("Ice Cube");
+        ep2.AdicionarConvidado("Dr. Dre");
+
+        Episodio ep3 = new(3, "Eminem and 50 Cent", 240);
+        ep3.AdicionarConvidado("Eminem");
+        ep3.AdicionarConvidado("50 Cent");
+
+        Podcast podcast = new("Sound System", "André Toti");
+        podcast.AdicionarEpisodio(ep1);
+        podcast.AdicionarEpisodio(ep2);
+        podcast.AdicionarEpisodio(ep3);
+        podcast.ExibirDetalhes();
+    }
+}
